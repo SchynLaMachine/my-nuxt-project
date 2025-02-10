cff09608d8376bb1b258318f6c73e8957deb30be
SchynLaMachine <nath.wick@gmail.com>
2025-02-10 19:09:35 +0100

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        external: ["/images/logo.png"], // Ajoute ton image ici
+      },
+    },
   },
 });
