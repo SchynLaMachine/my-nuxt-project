315915b2ed2930696d1b44765d79a24391782cba
SchynLaMachine <nath.wick@gmail.com>
2025-02-11 10:12:25 +0100

    Test2536

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
-
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
-    build: {
-      rollupOptions: {
-        external: ["/images/logo.png"], // Ajoute ton image ici
-      },
-    },
-  },
-  nitro: {
-    serveStatic: true, // Permet à Nitro de bien gérer les fichiers dans /public
-    externals: {
-      traceInclude: ["public/images/logo.png"], // Force l'inclusion de l'image dans le build
-    },
   },
 });
